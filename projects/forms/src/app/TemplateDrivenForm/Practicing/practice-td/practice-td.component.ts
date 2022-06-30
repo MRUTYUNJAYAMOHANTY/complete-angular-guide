5f92255b77d0be453386b6f92468de28a3e15293
Mrutyunjaya <me.mrutyunjaya@gmail.com>
2022-06-30 22:04:48 +0530

    routing added

---
+++ projects/forms/src/app/TemplateDrivenForm/Practicing/practice-td/practice-td.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-practice-td',
+  templateUrl: './practice-td.component.html',
+  styleUrls: ['./practice-td.component.css']
+})
+export class PracticeTdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- projects/forms/src/app/app.module.ts
+++ projects/forms/src/app/app.module.ts
@@ -4,14 +4,20 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { TemplateDrivenComponent } from './TemplateDrivenForm/template-driven/template-driven.component';
+import { PracticeTdComponent } from './TemplateDrivenForm/Practicing/practice-td/practice-td.component';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateDrivenComponent,
+    PracticeTdComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
