e6ec95d71cb4ba26880b6bf1f36fa57b5d9bf120
Mrutyunjaya <me.mrutyunjaya@gmail.com>
2022-07-01 20:47:01 +0530

    practice td

--- projects/forms/src/app/TemplateDrivenForm/Practicing/practice-td/practice-td.component.ts
+++ projects/forms/src/app/TemplateDrivenForm/Practicing/practice-td/practice-td.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-practice-td',
@@ -11,5 +12,10 @@ export class PracticeTdComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  selectedSubscription = 'Advanced';
+  @ViewChild('signupForm', { static: false }) sgnForm: NgForm;
+  onSubmit() {
+    console.log(this.sgnForm.value);
+  }
 }
