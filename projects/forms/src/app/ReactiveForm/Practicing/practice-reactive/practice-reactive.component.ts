5e78903b9ffb902b022acef2f258cda6d70dd20a
Mrutyunjaya <me.mrutyunjaya@gmail.com>
2022-07-13 12:02:42 +0530

    assignment reactive forms

--- projects/forms/src/app/ReactiveForm/Practicing/practice-reactive/practice-reactive.component.ts
+++ projects/forms/src/app/ReactiveForm/Practicing/practice-reactive/practice-reactive.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+import { CustomValidators } from './custom-validators';
 @Component({
   selector: 'app-practice-reactive',
   templateUrl: './practice-reactive.component.html',
@@ -9,7 +11,22 @@ export class PracticeReactiveComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  projectForm: FormGroup;
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(
+        null,
+        [Validators.required, CustomValidators.invalidProjectName],
+        CustomValidators.asyncInvalidProjectName
+      ),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus': new FormControl('critical')
+    });
+  }
+
+  onSaveProject() {
+    console.log(this.projectForm.value);
   }
 
 }
