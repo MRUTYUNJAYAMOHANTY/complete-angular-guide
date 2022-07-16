6267ed87c7f45de628d29023c4334002f17f5d8f
Mrutyunjaya <me.mrutyunjaya@gmail.com>
2022-07-16 12:48:57 +0530

    CRUD

--- projects/recipe_shopping-list/src/app/app.module.ts
+++ projects/recipe_shopping-list/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -16,6 +16,7 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeService } from './recipes/recipe.service';
 
 @NgModule({
   declarations: [
@@ -31,10 +32,13 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     RecipeStartComponent,
     RecipeEditComponent
   ],
-  imports: [BrowserModule, FormsModule,
-    HttpClientModule,AppRoutingModule,
-    ReactiveFormsModule],
-  providers: [ShoppingListService],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
+  providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- projects/recipe_shopping-list/src/app/shopping-list/shopping-list.service.ts
+++ projects/recipe_shopping-list/src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,5 @@
-import { Subject } from 'rxjs';
-
 import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs/Subject';
 
 export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
@@ -13,9 +12,11 @@ export class ShoppingListService {
   getIngredients() {
     return this.ingredients.slice();
   }
-  getIngredient(index:number){
-   return this.ingredients[index];
+
+  getIngredient(index: number) {
+    return this.ingredients[index];
   }
+
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
     this.ingredientsChanged.next(this.ingredients.slice());
@@ -28,12 +29,14 @@ export class ShoppingListService {
     this.ingredients.push(...ingredients);
     this.ingredientsChanged.next(this.ingredients.slice());
   }
-  updateIngredient(index:number,newIngredient:Ingredient){
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
     this.ingredients[index] = newIngredient;
-    this.ingredientsChanged.next(this.ingredients.slice()); 
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
-  deleteIngredients(index:number){
-    this.ingredients.splice(index,1);
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
